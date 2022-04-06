15ddf412e817047a60848d04eebcc4479100f3f5
Alex <alexhaigh23@outlook.com>
2022-04-06 16:26:53 +0100

    completed webpack setup

---
+++ webpack.prod.js
@@ -0,0 +1,62 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const path = require('path');
+const fse = require('fs-extra');
+
+const postcssPlugins = [
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('pstcss-simple-vars'),
+    require('postcss-mixins'),
+    require('autoprefixer')
+]
+
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.done.hooks.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './docs/assets/images');
+        })
+    }
+}
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, 'docs')
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        minimize: true,
+        minimizer: [`...`, new CssMinimizerPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postcssPlugins}}}]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'bael-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+    ]
+})
