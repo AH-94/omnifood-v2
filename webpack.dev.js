0a13e09043d7954c4c51eac9b03909b25b65ffc5
Alex <alexhaigh23@outlook.com>
2022-04-13 10:41:19 +0100

    completed hero section

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const path = require('path');
 const postcssPlugins = [
     require('postcss-import'),
     require('postcss-nested'),
-    require('pstcss-simple-vars'),
+    require('postcss-simple-vars'),
     require('postcss-mixins'),
     require('autoprefixer')
 ]
